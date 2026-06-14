5b46c8cf47768de604b2ed2f6334230109d2388f
Sumantkumar-gith <skjadon2@gmail.com>
2026-06-15 04:19:40 +0530

    02Datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS Code as newer version
+// alert(3+3)  --> we are using node.js not browser
+// console.log(3+3); console.log("lucky") ->code readability should be high
+let name = "lucky"
+let age = 18
+let isLoggedin = false
+// number => 2^53
+// bigint
+// string =>"" or ''
+// boolean => true or false
+// null => datatype and standalone value also
+// undefined => value is not assigned yet
+// symbol =>for uniqueness
+// object 
+//output -->
+console.log(typeof "lucky"); //string
+console.log(typeof true); //boolean
+console.log(typeof 1); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
