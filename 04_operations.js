fafab1d90d1aa055a00fef180742fda8634ac5d9
Sumantkumar-gith <skjadon2@gmail.com>
2026-06-15 12:33:49 +0530

    04operations

---
+++ 04_operations.js
@@ -0,0 +1,22 @@
+let value = 3
+let negValue = -value
+console.log(negValue)// -3
+
+// console.log(2+2) 4
+// console.log(2-2) 0
+// console.log(2*2) 4
+// console.log(2**2) 2^2=4
+// console.log(2/2) 1
+// console.log(2%2) 0
+
+let str1 = "hello"
+let str2 = " lucky"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log(1 + 2 + "2") //32 because number data type first that's why first we add the numbers and then add the string behind
+console.log("1" + 2 + 2) //122 because string datatype is first so when the next types as string also
+console.log(+true) //1
+console.log(+"") //0 -->type of unit conversion to number => (+dataType) = number
+
