3334b0ee76e091e742704c9ced05c8d54feca45d
Sumantkumar-gith <skjadon2@gmail.com>
2026-06-27 16:06:38 +0530

    question

---
+++ objectquestion.js
@@ -0,0 +1,32 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(Math.PI);
+console.log(descriptor);
+Math.PI.writable = true
+console.log(descriptor);
+
+const chai = {
+    name: "gingerchai",
+    price: 250,
+    isAvailable: true,
+
+    orderchai: function(){
+        console.log("chai nhi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: false,
+    
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}: ${value}`);
+    }
+}
+
